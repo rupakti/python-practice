c064cde9dd77ef2066b4eedf67f78ca4e102d00b
Rupak t <physik.rupak@gmail.com>
2020-06-22 16:58:47 -0700

    changed print s

--- python1.py
+++ python1.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Jun 22 11:19:51 2020
-
 @author: Rupak
 """
 
@@ -9,4 +7,6 @@ import cmath
 import random
 import tkinter
 import matplotlib
+
+
 print ("this is how we print")
