33484ae5bbc9928cf36329d2230ff598305941b0
Rupak t <physik.rupak@gmail.com>
2020-06-22 15:52:45 -0700

    First commit

---
+++ python1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 11:19:51 2020
+
+@author: Rupak
+"""
+
+import cmath
+import random
+import tkinter
+import matplotlib
+print ("this is how we print")
---
+++ strings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 12:31:40 2020
+
+@author: Rupak
+"""
+
+#strings
+full_name= "Rupak Timalsina"
+print(full_name.upper())
+print(full_name.title())
+
+first_name= "rupak"
+last_name= "timalsina"
+full_name1= first_name + last_name
+print(full_name1)
+
+print(4+2)
+print("4"+"2")
+      
+      
